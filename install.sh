43fa5e0565d07e3a3174f6b44343c4fe399c42b5
Woomymy <github@woomy.ovh>
2020-08-05 17:34:28 +0200

    Install script

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ if [ ! "$(command -v emerge)" ]; then
 	echo "This system is not a gentoo linux system"
 	exit 1
 fi
+if [[ ! "$(command -v git)" ]]; then
+	emerge git
+fi
+emerge eselect-repository
+eselect repository add woomy-overlay git https://github.com/woomy4680-exe/woomy-overlay.git
